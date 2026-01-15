7ac16857a2cec5d0016de3e975a8f2fe3eee3d51
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2026-01-15 15:57:47 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -80,6 +80,19 @@ const App = () => {
             <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
+          {trendingMovies.length > 0 && (
+            <section className="trending">
+              <h2>Trending Movies</h2>
+              <ul>
+                {trendingMovies.map((movie, index) => (
+                  <li key={movie.$id}>
+                    <p>{index + 1}</p>
+                    <img src={movie.poster_url} alt={movie.title} />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          )}
           <section className='all-movies'>
             <h2>All Movies</h2>
             {isLoading ? (
